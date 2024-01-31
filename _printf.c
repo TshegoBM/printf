08dd4bee5896e1e04018ab5bbc9e7a8ed270794b
“Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-01-31 03:55:53 -0800

    update

--- _printf.c
+++ _printf.c
@@ -8,32 +8,32 @@
   */
 int _printf(const char *format, ...)
 {
-	int n = 0, count = 0;
-	va_list args;
+        int n = 0, count = 0;
+        va_list args;
 
-	va_start(args, format);
-	while (format[n] != '\0')
-	{
-		if (format[n] == '%')
-		{
-			if (spec_f(format[n + 1]) == NULL)
-			{ /* no specifier after '%' continue normally */
-				write(1, (format + n), 1);
-				count++;
-				n++;
-			}
-			else
-			{ /* call function to specifier */
-				count += (*spec_f(format[n + 1]))(&args);
-				n += 2; /* skip to character after specifier */
-			}
-			continue;
-		}
-		write(1, (format + n), 1);
-		count++;
-		n++;
+        va_start(args, format);
+        while (format[n] != '\0')
+        {
+                if (format[n] == '%')
+                {
+                        if (spec_f(format[n + 1]) == NULL)
+                        { /* no specifier after '%' continue normally */
+                                write(1, (format + n), 1);
+                                count++;
+                                n++;
+                        }
+                        else
+                        { /* call function to specifier */
+                                count += (*spec_f(format[n + 1]))(&args);
+                                n += 2; /* skip to character after specifier */
+                        }
+                        continue;
+                }
+                write(1, (format + n), 1);
+                count++;
+                n++;
 
-	}
-	va_end(args);
-	return (count);
+        }
+        va_end(args);
+        return (count);
 }
